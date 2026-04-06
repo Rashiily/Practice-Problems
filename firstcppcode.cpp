ef2f21e6b2a308f997cba4db2ab8892f4ca3d935
Rashiily <rashipatel2329@gmail.com>
2026-04-06 14:25:59 +0530

    Add files via upload

---
+++ firstcppcode.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<" Hello World ";
+    return 0;
+}
+ 
+
+#include<iostream>
+using namespace std;
+int main ()
+{
+    cout<<" Hello world ";
+    return 0;
+}
